86339bc8b035a194811757dc365965715acfd978
Keks <keks@htmlacademy.ru>
2024-03-30 16:09:38 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/main.js
+++ 12/js/main.js
@@ -2,18 +2,17 @@ import { renderThumbnails } from './thumbnails.js';
 import { gallery } from './gallery.js';
 import { setFormSubmit } from './validate-form.js';
 import { getData } from './api.js';
-import { showGalleryErrorMessage } from './message.js';
-import { showFilters, onFiltersClick } from './filters.js';
+import { dataErrorMessage } from './message.js';
+import { initFilter } from './filter.js';
 
 getData()
   .then((photos) => {
     renderThumbnails(photos);
     gallery(photos);
-    showFilters();
-    onFiltersClick(photos);
+    initFilter(photos);
   })
   .catch(() => {
-    showGalleryErrorMessage();
+    dataErrorMessage();
   });
 
 setFormSubmit();
